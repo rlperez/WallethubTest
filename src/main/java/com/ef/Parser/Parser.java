58b388698dfce8e9cbf9d2e43917fb8c8d0a7271
Rigoberto L. Perez <rperez@demandbridge.com>
2018-07-10 14:24:55 -0400

    Add file path with default

--- src/main/java/com/ef/Parser/Parser.java
+++ src/main/java/com/ef/Parser/Parser.java
@@ -12,19 +12,23 @@ public class Parser {
     private static final String DURATION_KEY = "duration";
     private static final String THRESHOLD_KEY = "threshold";
     private static final String START_DATE_KEY = "startDate";
+    private static final String FILE_PATH_KEY = "file";
 
     public static void main(String[] args) {
         Map<String, String> parsedArguments = Arrays.stream(args)
                 .filter(arg -> arg.startsWith("--"))
                 .map(arg -> arg.split("="))
-                .collect(Collectors.toMap(arg -> arg[0].replaceFirst("--", ""), arg -> arg[1].toUpperCase()));
+                .collect(Collectors.toMap(arg ->
+                        arg[0].replaceFirst("--", ""),
+                        arg -> arg[1].toUpperCase()));
 
         try {
-            Duration duration = Duration.valueOf(parsedArguments.get(DURATION_KEY));
-            int threshold = Integer.parseInt(parsedArguments.get(THRESHOLD_KEY));
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd.HH:mm:ss");
             LocalDate date = LocalDate.parse(parsedArguments.get(START_DATE_KEY), formatter);
-                        
+            Duration duration = Duration.valueOf(parsedArguments.get(DURATION_KEY));
+            int threshold = Integer.parseInt(parsedArguments.get(THRESHOLD_KEY));
+            String filePath = parsedArguments.getOrDefault("file", "access.log");
+
         } catch (IllegalArgumentException | DateTimeParseException exception) {
             exception.printStackTrace();
         }

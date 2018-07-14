8be24fd7787ff603683879955d351afadb00a7fe
Rigoberto L. Perez <rlperez@udel.edu>
2018-07-13 22:55:54 -0400

    Make some columns not null and fix the requested sql queries

--- src/main/resources/meh.sql
+++ src/main/resources/meh.sql
@@ -1,2 +1,7 @@
-select * from log_entry where ip = ?;
-select * from blocked_ip_log_entry where request_count > 100;
+select ip from (select ip, count(1) as count
+  from log_entry
+  where TIMESTAMP('2017-01-01.17:00:00') < timestamp and
+        TIMESTAMP('2017-01-01.18:00:00') > timestamp
+  group by ip) as time_spanned_log where count > 100;
+
+select * from log_entry where ip = '192.168.230.70';
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,8 @@ create table wallethub.blocked_ip_log_entry
 (
 	id bigint not null
 		primary key,
-	comment varchar(255) null,
-	ip varchar(255) null,
+	comment varchar(255) not null,
+	ip varchar(255) not null,
 	request_count bigint not null
 );
 
@@ -11,11 +11,11 @@ create table wallethub.log_entry
 (
 	id bigint not null
 		primary key,
-	ip varchar(255) null,
-	request varchar(255) null,
+	ip varchar(255) not null,
+	request varchar(255) not null,
 	response_code int not null,
-	timestamp datetime null,
-	user_agent varchar(255) null
+	timestamp datetime not null,
+	user_agent varchar(255) not null
 );
 
 
